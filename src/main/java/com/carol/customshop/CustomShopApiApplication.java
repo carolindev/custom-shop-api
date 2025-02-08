14d7039b78cc1bd97639fc20256225a6753d5007
Carolina <carolina@applimarc.com>
2025-02-08 20:04:00 +0100

    Add basic config

---
+++ src/main/java/com/carol/customshop/CustomShopApiApplication.java
@@ -0,0 +1,11 @@
+package com.carol.customshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomShopApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomShopApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/carol/customshop/CustomShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carol.customshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test ensures the Spring application context loads successfully
+    }
+}
